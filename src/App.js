af5e30e9aff1ea227ec2c0cfa2897756541b3a38
Angelo <ljy6037@gmail.com>
2023-10-31 16:34:36 +0900

    231031 인라인 스타일 적용하는 방법 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
 function App() {
+  let MyStyle = {
+    display: "flex",
+    justifyContent: "space-evenly",
+  };
   return (
-    <div>
-
+    <div style={MyStyle}>
+      <MyComp />
+      <MyComp />
+      <MyComp />
     </div>
   );
+}
+
+function MyComp() {
+  const name = "흥민";
+  return <div style={{ backgroundColor: "black", color: "white" }}>{name}</div>;
 }
 
 export default App;
