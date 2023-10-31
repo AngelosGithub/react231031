096f3621b0c4271f690966b927b1f629ffb2d60c
Angelo <ljy6037@gmail.com>
2023-10-31 12:15:04 +0900

    231031 component 연습, 작성 완료

--- src/App.js
+++ src/App.js
@@ -2,9 +2,46 @@
 function App() {
   return (
     <div>
-
+        <MyComponent1 />
+        {/*
+        <div>
+            <h1>Hello Component</h1>
+        </div>
+        */}
+        <MyComponent2 />
+        {/*
+            <>
+                <p>Lorem ipsum dolor sit.</p>
+                <ul>
+                    <li>Lorem.</li>
+                    <li>Excepturi?</li>
+                    <li>Voluptates.</li>
+                </ul>
+            </>
+        */}
     </div>
   );
+}
+
+function MyComponent1() {
+    return (
+        <div>
+            <h1>Hello Component</h1>
+        </div>
+    )
+}
+
+function MyComponent2() {
+    return(
+        <>
+            <p>Lorem ipsum dolor sit.</p>
+            <ul>
+                <li>Lorem.</li>
+                <li>Excepturi?</li>
+                <li>Voluptates.</li>
+            </ul>
+        </>
+    )
 }
 
 export default App;
