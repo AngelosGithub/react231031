6f25ee924cdd33e7941ffb408e9c3d0bfe37e29f
Angelo <ljy6037@gmail.com>
2023-10-31 11:40:16 +0900

    231031 component, 주석, jsx

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@
 function App() {
   return (
     <div>
-
+      <h1>hello react</h1>
+        <h2>HI REACT</h2>
+        <MyComp />
     </div>
   );
+}
+
+// built-in component 는 소문자 시작
+// component 명은 대문자 시작
+// return 문을 가져야함
+// return 값은 보통 JSX 코드
+
+function MyComp() {
+    // return 에 jsx 코드 작성가능
+    // 꼭 하나의 root tag가 존재 해야함
+    // return 값이 여러줄이면 ()로 감싸기
+    // root tag 가 필요 없다면 fragment 사용 가능
+
+    // jxs 주석 사용법
+    // {/*  */}
+    return (
+        <>
+            {/* fregment 는 <></> */}
+            <h1>HI COMPONENT</h1>
+            <h2>hello</h2>
+        </>
+    )
 }
 
 export default App;
