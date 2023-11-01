c655a467c7c82baec5c5705426ce31efe07af78b
Angelo <ljy6037@gmail.com>
2023-11-01 11:22:28 +0900

    231101 props 구조분해할당 기본값 주기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 function App() {
   return (
     <>
-      <MyComp value="흥민" address="신촌" />
-      <MyComp value="강인" address="강남" />
+      <MyComp name="흥민" address="신촌" age={33} city="서울" />
+      <MyComp address="강남" age={33} city="부산" />
+      <MyComp address="강남" city="부산" />
     </>
   );
 }
 
-function MyComp({ value, address }) {
+function MyComp({ name = "불명", address, city, age = 0 }) {
   return (
     <div>
-      <h1>
-        {value}이는 {address}에 산다
-      </h1>
+      <p>
+        {name}은 {age}살 이고 {address}, {city}에 산다
+      </p>
     </div>
   );
 }
