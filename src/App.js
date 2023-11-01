25629e1e7410bf519232008172d6c850517a32ff
Angelo <ljy6037@gmail.com>
2023-11-01 14:16:06 +0900

    231101 default export, import

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { MyBox, MyElem, MyContainer, name, person } from "./component/MyBox";
+import { MyElem, city } from "./component/MyElem";
+import age123 from "./component/MyElem";
+import MyBox123, { person, country } from "./component/MyBox";
+// 꼭 따로 적을 필요는 없음
+// import {person, country} from "./component/MyBox"
 
 function App() {
-  console.log(name, "name");
-  console.log(person, "person");
   return (
     <>
-      <MyBox />
-      <MyElem />
-      <MyContainer />
+      <h1>{city}</h1>
+      <h1>{age123}</h1>
+      <MyBox123 />
+      <h1>
+        {person.age}, {person.name}
+      </h1>
+      <h1>{country}</h1>
     </>
   );
 }
---
+++ src/component/MyElem.js
@@ -0,0 +1,18 @@
+// named expert
+export function MyElem() {
+  return (
+    <div>
+      <h1>My Element</h1>
+    </div>
+  );
+}
+
+// named expert
+export const city = "seoul";
+
+const age = 33;
+
+// default export
+// 이름이 정해져 있지 않음
+// 한 파일에 하나만 가능
+export default age;
