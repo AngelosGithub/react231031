b9466d12639637e16928f61214a28f0bf79b7cf9
Angelo <ljy6037@gmail.com>
2023-11-02 11:34:07 +0900

    231102 SimpleGrid 사용 반응형 웹 적용

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Flex
-        w={"100%"}
-        h={"100px"}
-        bg={"black"}
-        gap={5}
-        align={"center"}
-        justify={"space-evenly"}
-      >
-        <Box bg={"gold"}>Lorem.</Box>
-        <Box bg={"blue"}>Fugiat?</Box>
-        <Box bg={"yellow"}>Tempora!</Box>
-        <Box bg={"white"}>Harum!</Box>
-        <Box bg={"gray"}>Officia.</Box>
-      </Flex>
+      <SimpleGrid gap={3} columns={{ sm: 2, md: 3, "2xl": 5 }}>
+        <Box bg="blue.300">Lorem.</Box>
+        <Box bg="blue.300">Alias.</Box>
+        <Box bg="blue.300">Ratione?</Box>
+        <Box bg="blue.300">Non?</Box>
+        <Box bg="blue.300">Suscipit!</Box>
+        <Box bg="blue.300">Ipsam!</Box>
+        <Box bg="blue.300">Iste!</Box>
+        <Box bg="blue.300">Nisi.</Box>
+        <Box bg="blue.300">Recusandae.</Box>
+        <Box bg="blue.300">Minima!</Box>
+        <Box bg="blue.300">Ut!</Box>
+        <Box bg="blue.300">Cumque!</Box>
+        <Box bg="blue.300">Necessitatibus?</Box>
+        <Box bg="blue.300">Inventore!</Box>
+        <Box bg="blue.300">Possimus.</Box>
+        <Box bg="blue.300">Odio.</Box>
+        <Box bg="blue.300">Quos.</Box>
+        <Box bg="blue.300">Consequuntur!</Box>
+        <Box bg="blue.300">Quibusdam?</Box>
+        <Box bg="blue.300">At?</Box>
+        <Box bg="blue.300">Quisquam!</Box>
+        <Box bg="blue.300">Praesentium.</Box>
+        <Box bg="blue.300">Labore.</Box>
+        <Box bg="blue.300">Magni!</Box>
+        <Box bg="blue.300">Dolor.</Box>
+        <Box bg="blue.300">Doloribus!</Box>
+        <Box bg="blue.300">Incidunt?</Box>
+        <Box bg="blue.300">Doloribus!</Box>
+        <Box bg="blue.300">Fugiat?</Box>
+        <Box bg="blue.300">Aperiam?</Box>
+      </SimpleGrid>
     </>
   );
 }
