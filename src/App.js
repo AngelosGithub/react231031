ef081c9fbdc28fb4522c205a86f5db04304bd4a5
Angelo <ljy6037@gmail.com>
2023-11-02 14:35:00 +0900

    231102 Button 사용

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
-import { Button, useToast } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { ArrowRightIcon, EmailIcon } from "@chakra-ui/icons";
 
 function App() {
-  const toast = useToast();
   return (
     <>
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장 완료",
-            description: "게시물 저장 되었습니다",
-            duration: 2000,
-            position: "top",
-            isClosable: true,
-          })
-        }
-      >
-        토스트 띄우기
+      <Button colorScheme="blue">Lorem.</Button>
+      <Button colorScheme="yellow">Lorem.</Button>
+      <Button colorScheme="orange">Mollitia!</Button>
+      <Button size={"sm"} colorScheme="green">
+        Fugiat.
       </Button>
-
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장 실패",
-            description: "게시물 저장 되지 않았습니다",
-            duration: 2000,
-            position: "top",
-            isClosable: true,
-            status: "error",
-          })
-        }
-      >
-        토스트 띄우기
+      <Button size={"lg"} colorScheme="teal">
+        Dolore.
       </Button>
+      <Button colorScheme="telegram" variant={"outline"}>
+        Ut.
+      </Button>
+      <Button colorScheme="blackAlpha" variant={"ghost"}>
+        Exercitationem?
+      </Button>
+      <Button variant={"link"}>Quibusdam?</Button>
+      <Button leftIcon={<EmailIcon />}>Quam.</Button>
+      <Button rightIcon={<ArrowRightIcon />}>Consectetur!</Button>
+      <Button isLoading>Voluptatem!</Button>
+      <Button isLoading loadingText={"전송중"}>
+        Ad.
+      </Button>
+      <Button>Facere.</Button>
+      <Button>Recusandae?</Button>
+      <Button>Eligendi.</Button>
+      <Button>Animi!</Button>
+      <Button>Nostrum?</Button>
+      <Button>Velit.</Button>
+      <Button>Quo.</Button>
+      <Button>Nisi.</Button>
+      <Button>Ullam!</Button>
+      <Button>Molestiae.</Button>
+      <Button>Delectus.</Button>
+      <Button>At.</Button>
+      <Button>Ipsam.</Button>
+      <Button>Minima.</Button>
+      <Button>Blanditiis?</Button>
+      <Button>Eligendi.</Button>
+      <Button>Aliquam.</Button>
+      <Button>Placeat.</Button>
+      <Button>Ex!</Button>
     </>
   );
 }
