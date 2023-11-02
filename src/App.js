5dd52a02d58c4d9885ad9792a46161ca5cf59c9f
Angelo <ljy6037@gmail.com>
2023-11-02 09:34:43 +0900

    231102 불필요한 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
-// style 적용 방법
-// 보통 css 파일
-// css module 사용
-// chakra ui css library
-
-import { MyBox } from "./component/MyBox";
-import { MyComp } from "./component/MyComp";
-import { MyComponent } from "./component/MyComponent";
-
 function App() {
-  return (
-    <>
-      <MyComp />
-      <MyBox />
-      <MyComponent />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
