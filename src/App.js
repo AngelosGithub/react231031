2a6d4a9d80c9219337a937203808d1932549cec9
Angelo <ljy6037@gmail.com>
2023-11-02 10:23:44 +0900

    231102 필요없는 코드 정리

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-import { Box } from "@chakra-ui/react";
-
 function App() {
-  return (
-    <>
-      <Box bgColor="tomato">Lorem ipsum dolor.</Box>
-      <Box bgColor="red">Lorem ipsum dolor.</Box>
-      <Box bgColor="blue.900">Aperiam, autem soluta?</Box>
-      <Box bgColor="blue.200">Beatae doloremque, explicabo.</Box>
-      <Box color="pink">Ad, consectetur, tempore?</Box>
-      <Box color="red.200">Adipisci dignissimos, harum.</Box>
-      <Box>Fugiat, porro sit.</Box>
-      <Box>Explicabo, mollitia, recusandae.</Box>
-      <Box>Explicabo, maiores, molestiae.</Box>
-      <Box>Deserunt, nemo, omnis?</Box>
-      <Box>Doloribus, magnam, molestias!</Box>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
