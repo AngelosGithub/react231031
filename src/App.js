01202d2aa26916b27fb784394e277baedd1a4538
Angelo <ljy6037@gmail.com>
2023-11-03 16:55:10 +0900

    231103 state 활용 예제

--- src/App.js
+++ src/App.js
@@ -1,16 +1,67 @@
 import React, { useState } from "react";
-import { Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 
 function App(props) {
-  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [submit, setSubmit] = useState(false);
 
-  function handleChange(e) {
-    setMessage(e.target.value);
+  function handleNameInput(e) {
+    setName(e.target.value);
   }
+  function handlePWInput(e) {
+    setPassword(e.target.value);
+  }
+  function handleEmailInput(e) {
+    setEmail(e.target.value);
+  }
+  function handleSubmit() {
+    setSubmit(true);
+  }
+  //   axios
+  //     .post("/adduser", {
+  //       name,
+  //       email,
+  //       password,
+  //     })
+  //     .then((response) => console.log("성공"))
+  //     .catch((error) => console.log("실패"))
+  //     .finally(() => console.log("필수입력"));
+  // }
   return (
-    <div>
-      <Input type="text" value={message} onChange={handleChange} />
-    </div>
+    <Center>
+      <Box w={"480px"}>
+        <FormControl mb={5}>
+          <FormLabel>Name</FormLabel>
+          <Input type="text" value={name} onChange={handleNameInput} />
+          <FormHelperText>띄어쓰기 없이 입력해주세요.</FormHelperText>
+        </FormControl>
+
+        <FormControl mb={5}>
+          <FormLabel>Password</FormLabel>
+          <Input type="password" value={password} onChange={handlePWInput} />
+        </FormControl>
+
+        <FormControl mb={5}>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" value={email} onChange={handleEmailInput} />
+          <FormHelperText>이메일은 중복될 수 없습니다.</FormHelperText>
+        </FormControl>
+
+        <Button colorScheme="blue" onClick={handleSubmit} isLoading={submit}>
+          가입
+        </Button>
+      </Box>
+    </Center>
   );
 }
 
